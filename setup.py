ded2e328ef784c208f67f4b9ab761402541828fc
Radu Suciu <radusuciu@gmail.com>
2018-07-25 17:09:03 -0700

    Auto dependency link gen

--- setup.py
+++ setup.py
@@ -14,6 +14,39 @@ def read(*parts):
 
 LONG_DESCRIPTION = read('README.rst')
 
+def install_deps():
+    """Reads requirements.txt and preprocess it
+    to be feed into setuptools.
+
+    This is the only possible way (we found)
+    how requirements.txt can be reused in setup.py
+    using dependencies from private github repositories.
+
+    Links must be appendend by `-{StringWithAtLeastOneNumber}`
+    or something like that, so e.g. `-9231` works as well as
+    `1.1.0`. This is ignored by the setuptools, but has to be there.
+
+    Warnings:
+        to make pip respect the links, you have to use
+        `--process-dependency-links` switch. So e.g.:
+        `pip install --process-dependency-links {git-url}`
+
+    Returns:
+         list of packages and dependency links.
+    """
+    default = read('requirements.txt').readlines()
+    new_pkgs = []
+    links = []
+    for resource in default:
+        if 'git+ssh' in resource:
+            pkg = resource.split('#')[-1]
+            links.append(resource.strip() + '-9876543210')
+            new_pkgs.append(pkg.replace('egg=', '').rstrip())
+        else:
+            new_pkgs.append(resource.strip())
+    return new_pkgs, links
+
+pkgs, new_links = install_deps()
 
 setup(
     name='ip2api',
@@ -21,7 +54,8 @@ setup(
     url='http://github.com/radusuciu/ip2api/',
     license='Apache Software License',
     author='Radu Suciu',
-    install_requires=['beautifulsoup4', 'requests', 'polling'],
+    install_requires=pkgs,
+    dependency_links=new_links,
     author_email='radusuciu@gmail.com',
     description='Simple interface for IP2',
     long_description=LONG_DESCRIPTION,
